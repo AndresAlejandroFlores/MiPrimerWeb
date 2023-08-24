2b2d0de3c8bc6bd5e0fbff8b18d37ce203d0032a
AndresAlejandroFlores <143041474+AndresAlejandroFlores@users.noreply.github.com>
2023-08-24 12:53:36 -0300

    MiPrimerWeb

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function efecto(){
+    let aux = document.querySelector('.toggle');
+    if (aux.style.display=="none"){
+        aux.style.display="inline-block";
+    }
+    else
+    {
+        aux.style.display="none";
+    }
+}
+

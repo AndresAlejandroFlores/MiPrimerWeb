4964cbac5012ceed6dcee5d351cc126db0b5e4bb
AndresAlejandroFlores <143041474+AndresAlejandroFlores@users.noreply.github.com>
2023-08-25 01:46:51 -0300

    MiPrimerWeb

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-
+let visible = false;
 function efecto(){
-    let aux = document.querySelector('.toggle');
-    if (aux.style.display=="none"){
-        aux.style.display="inline-block";
+    let aux = document.querySelector('.ocultar');
+    aux.addEventListener('click', mostrarmenu);
+}
+function mostrarmenu(){
+    let aux = document.querySelector('.listado');
+    if (!visible){
+        aux.style.display = "flex";
+        visible = true;
     }
     else
     {
         aux.style.display="none";
+        visible = false;
     }
 }
+window.addEventListener('load', efecto);
 
